42f24589cb8ea9f0844df79d7d02e6b944953027
Estudiante <estudiante@UCUXA40202.continental.edu.pe>
2024-05-07 15:13:29 -0500

    proyecto

---
+++ Models/Pagos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using backend.Models;
+
+namespace backend.Models
+{
+    public class Pagos
+    {
+        [Key]
+        public int id_pago { get; set; }
+
+        public int id_reserva { get; set; }
+
+        [ForeignKey("id_reserva")]
+        public Reservas reserva { get; set; }
+
+        [Required]
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal monto { get; set; }
+
+        public DateTime fecha_pago { get; set; }
+    }
+}
---
+++ Models/Reservas.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using backend.Models;
+
+namespace backend.Models
+{
+    public class Reservas
+    {
+        [Key]
+        public int id_reserva { get; set; }
+
+        public int id_tour { get; set; }
+
+        [ForeignKey("IdTour")]
+        public Tours tour { get; set; }
+
+        [StringLength(12)]
+        public string cliente { get; set; }
+
+        [ForeignKey("IdCliente")]
+        public Clientes Cliente { get; set; }
+
+        public DateTime FechaReserva { get; set; }
+
+        public int CantidadPersonas { get; set; }
+    }
+}
